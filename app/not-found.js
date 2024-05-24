fc81f7e4c21ee0665f715bf54b68b258cb9e1f46
asib ahmed <ahmedasib1@gmail.com>
2024-05-24 16:21:02 +0600

    feat: customize page not found

---
+++ app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notfound = () => {
+    return (
+        <div>
+            the requested response is not found
+        </div>
+    );
+};
+
+export default notfound;
