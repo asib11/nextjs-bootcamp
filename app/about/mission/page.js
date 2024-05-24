ca57f484c1bdc638a464c941a85a2b560b32581c
asib ahmed <ahmedasib1@gmail.com>
2024-05-24 15:35:43 +0600

    fix: nested layout page

---
+++ app/about/mission/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <main className="">
+            <div className="mt-10">
+                <h1 className="text-7xl">Mission</h1>
+                this is Mission page
+            </div>
+        </main>
+    );
+};
+
+export default page;
