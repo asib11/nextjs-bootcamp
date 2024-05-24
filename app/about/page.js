ca3c92bf03e30373185f37ee9c59c2addbf4b384
asib ahmed <ahmedasib1@gmail.com>
2024-05-24 14:56:27 +0600

    feat: learn pages and layout

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+
+export default function About() {
+    return (
+      <main className="">
+        <div className="mt-10">
+            this is about us page
+        </div>
+      </main>
+    );
+  }
