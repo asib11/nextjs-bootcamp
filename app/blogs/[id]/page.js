8f393f2192ee35b0202aa490ddd6aace2b203b7c
asib ahmed <ahmedasib1@gmail.com>
2024-05-24 18:18:53 +0600

    feat: inner page not found set

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,7 +1,10 @@
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 const Blog = ({params}) => {
     const {id} = params;
+
+    if(id=== '4') notFound();
     return (
         <div className='mt-6'>
             the blog id is {id}
