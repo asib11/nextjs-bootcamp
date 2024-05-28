6d76e18d6f86b7fa617b1e5deba7d09a3df76d87
asib ahmed <ahmedasib1@gmail.com>
2024-05-28 14:56:09 +0600

    feat: dynamic meta data

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,15 @@
 import getPost from '@/lib/getPost';
 import React from 'react';
 
+export const generateMetadata = async ({ params }) =>{ // dynamic meta data
+    const { id } = params;
+    const post = await getPost(id);
+    return {
+        title: post.title,
+        description: post.body
+    }
+}
+
 const page = async ({ params }) => {
     const { id } = params;
     const post = await getPost(id);
