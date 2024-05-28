3ef269746f4d0c4ce80296248b2734ea24c86138
asib ahmed <ahmedasib1@gmail.com>
2024-05-28 16:52:24 +0600

    feat: promise to api together

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,9 +1,12 @@
 import getPost from '@/lib/getPost';
+import getPostComments from '@/lib/getPostComments';
 import React from 'react';
 
-export const generateMetadata = async ({ params }) =>{ // dynamic meta data
+export const generateMetadata = async ({ params }) => { // dynamic meta data
     const { id } = params;
     const post = await getPost(id);
+    // console.log(comments)
+
     return {
         title: post.title,
         description: post.body
@@ -12,12 +15,23 @@ export const generateMetadata = async ({ params }) =>{ // dynamic meta data
 
 const page = async ({ params }) => {
     const { id } = params;
-    const post = await getPost(id);
+    const postPromise = getPost(id);
+    const commentsPromise = getPostComments(id)
+    const [post, comments] = await Promise.all([postPromise, commentsPromise])
 
     return (
         <>
-            <h1 className='text-2xl'>{post.title}</h1>
-            <p className='mt-3'>{post.body}</p>
+            <div className='text-blue-500'>
+                <h1 className='text-2xl'>{post.title}</h1>
+                <p className='mt-3'>{post.body}</p>
+            </div>
+            <div className='mt-10'>
+                <ul>
+                    {
+                        comments.map(commnet => <li className='mt-6' key={commnet.id}>{commnet.body}</li>)
+                    }
+                </ul>
+            </div>
         </>
     );
 };
